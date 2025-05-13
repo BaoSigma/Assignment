8d7f78f007c453c752b0440793c26b850b28e8bd
BaoSigma <baohakura@gmail.com>
2025-05-13 22:10:55 +0700

    Ass

--- src/poly/cafe/entity/TheDinhDanh.java
+++ src/poly/cafe/entity/TheDinhDanh.java
@@ -3,11 +3,33 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package poly.cafe.entity;
-
 /**
  *
  * @author baoha
  */
 public class TheDinhDanh {
+    private String maDD;
+    private String trangThai;
+
+    public TheDinhDanh(String maDD, String trangThai) {
+        this.maDD = maDD;
+        this.trangThai = trangThai;
+    }
+
+    public String getMaDD() {
+        return maDD;
+    }
+
+    public void setMaDD(String maDD) {
+        this.maDD = maDD;
+    }
+
+    public String getTrangThai() {
+        return trangThai;
+    }
+
+    public void setTrangThai(String trangThai) {
+        this.trangThai = trangThai;
+    }
     
 }
